18666bfa1bcdaab57279a8f7d3ffd9657dbc746a
15009114725@163.com <15009114725@163.com>
2020-02-15 12:23:21 +0800

    无变化

--- 25.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-int i,s=1;
-for(i=0;i<9;i++)
-{
-	s=(s+1)*2;
-}
-printf("The first day:%d\n",s);
- }
--- 46.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-	int i,t=0;
-	for(i=1;i<=100;i++)
-	t+=i; 
-	printf("1~100µÄºÍÎª%d",t);
- } 
