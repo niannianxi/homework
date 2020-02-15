a8283e2836de39f25f965d922db246e6d0d51d8f
niannianxi <57067438+niannianxi@users.noreply.github.com>
2020-02-15 14:31:36 +0800

    Update 83.c

--- 83.c
+++ 83.c
@@ -1,11 +1,21 @@
-/*83.��Ŀ���Ӽ�������һЩ�ַ������������д�������ļ��ϣ�ֱ
-������һ��#Ϊֹ��*/
+/*83.题目：从键盘输入一些字符，逐个把它们写到磁盘文件上，直
+到输入一个#为止。*/
 #include<stdio.h>
 #include<string.h>
 int main(void)
 {
 	FILE *fp;
 	fp=fopen("1.txt","w");
+
+if((fp=fopen("1.txt","w"))==NULL)//打开输出文件并使fp指向此文件
+
+{
+
+   printf("无法打开此文件!\n");//如果打开出错就输出打不开的信息
+
+   exit(0);//退出程序
+
+}
 	char c;
 	scanf("%c",&c);
 	while(c!='#')
@@ -13,5 +23,6 @@ int main(void)
 		fprintf(fp,"%c",c);
 		scanf("%c",&c);
 	}
+        fclose(fp);
 	return 0;
 }
